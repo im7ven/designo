b0da326c3fa6c90c16394f51c5d5f929d1d3b250
Justin <alexanderrr416@gmail.com>
2024-08-01 01:53:23 -0400

    refactor: replace heading with a next link

--- app/components/shared/Navbar.tsx
+++ app/components/shared/Navbar.tsx
@@ -2,15 +2,14 @@ import Image from "next/image";
 import logo from "@/public/images/shared/desktop/logo-dark.png";
 import NavList from "./NavList";
 import { useState } from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="flex flex-wrap justify-between items-center py-9 px-6 relative z-10">
-      <h1>
-        <a href="/">
-          <Image width={202} src={logo} alt="logo" />
-        </a>
-      </h1>
+      <Link href="/">
+        <Image width={202} src={logo} alt="logo" />
+      </Link>
       <NavList />
     </nav>
   );
