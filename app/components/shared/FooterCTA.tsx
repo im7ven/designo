a68d3a36544231f031dd0e2be503699bdc53f2c7
Justin <alexanderrr416@gmail.com>
2024-08-06 16:37:28 -0400

    feat: build CTA footer component

---
+++ app/components/shared/FooterCTA.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+const FooterCTA = () => {
+  return (
+    <div className="px-[2.4rem]">
+      <section className="footer-cta text-center bg-[--peach-dark] rounded-xl py-[6.4rem] px-[2.4rem] md:py-[5.7rem] lg:py-[7.2rem] lg:px-[9.5rem]  lg:flex lg:items-center lg:text-left lg:justify-between">
+        <div className="max-w-[44.9rem] mx-auto lg:mx-[initial]">
+          <h2 className="tracking-normal leading-[3.6rem]">
+            Let's talk about your project
+          </h2>
+          <p className="body-text text-white pt-[1.6rem] pb-[3.2rem] lg:pb-0">
+            Ready to take it to the next level? Contact us today and find out
+            how our expertise can help your business grow.
+          </p>
+        </div>
+        <Link href="/" className="btn-primary">
+          Get in touch
+        </Link>
+      </section>
+    </div>
+  );
+};
+
+export default FooterCTA;
