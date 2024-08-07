a127f7bb2ebdaa2edb971e1ae54e8fb91e49e895
Justin <alexanderrr416@gmail.com>
2024-08-06 22:01:53 -0400

    style: apply max width to cta

--- app/components/shared/FooterCTA.tsx
+++ app/components/shared/FooterCTA.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const FooterCTA = () => {
   return (
     <div className="px-[2.4rem]">
-      <section className="footer-cta text-center bg-[--peach-dark] rounded-xl py-[6.4rem] px-[2.4rem] md:py-[5.7rem] lg:py-[7.2rem] lg:px-[9.5rem]  lg:flex lg:items-center lg:text-left lg:justify-between">
+      <section className="footer-cta text-center bg-[--peach-dark] rounded-xl py-[6.4rem] px-[2.4rem] md:py-[5.7rem] max-w-[111rem] lg:mx-auto lg:py-[7.2rem] lg:px-[9.5rem]  lg:flex lg:items-center lg:text-left lg:justify-between">
         <div className="max-w-[44.9rem] mx-auto lg:mx-[initial]">
           <h2 className="tracking-normal leading-[3.6rem]">
             Let's talk about your project
