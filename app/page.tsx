13b626312d39f295c1847a0b032f90765f0fb7b3
Justin <alexanderrr416@gmail.com>
2024-08-06 23:10:10 -0400

    style: add background design elements

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,28 @@ import FeatureCard from "./components/home/FeatureCard";
 import HomeHero from "./components/home/HomeHero";
 import DesignCard from "./components/shared/DesignCard";
 import Footer from "./components/shared/Footer";
-import FooterCTA from "./components/shared/FooterCTA";
+import Image from "next/image";
 import { appDesign, graphicDesign, webDesignLg } from "./data/designCards";
 import {
   friendlyCard,
   passionateCard,
   resourcefulCard,
 } from "./data/featureCards";
+import backgroundDesign from "@/public/images/shared/desktop/bg-pattern-leaf.svg";
 
 export default function Home() {
   return (
     <main>
       <div className=" max-w-[111rem] box-content mx-auto md:px-16">
         <HomeHero />
+        <div className="background-design1" />
         <section className="mt-[12rem] grid gap-[2.4rem] px-[2.4rem] max-w-[68.9rem] md:px-0 mx-auto lg:max-w-[initial] lg:grid-cols-2 lg:grid-rows-2 lg:mt[16rem]">
           <DesignCard {...webDesignLg} />
           <DesignCard {...appDesign} />
           <DesignCard {...graphicDesign} />
         </section>
+        <div className="background-design2" />
+
         <section className="flex flex-col gap-[8rem] px-[2.4rem] mt-[12rem] md:px-0 md:gap-[3.2rem] lg:flex-row lg:gap-[3rem] lg:mt-[16rem]">
           <FeatureCard {...passionateCard} />
           <FeatureCard {...resourcefulCard} />
