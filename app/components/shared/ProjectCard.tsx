9b19d64d601e85156de62ab75963dcdb446fe33c
Justin <alexanderrr416@gmail.com>
2024-08-07 21:18:46 -0400

    feat: build project card component

---
+++ app/components/shared/ProjectCard.tsx
@@ -0,0 +1,22 @@
+import { ProjectCardProps } from "@/app/data/webDesignCards";
+import Image from "next/image";
+
+const ProjectCard = ({ ...props }: ProjectCardProps) => {
+  return (
+    <article className=" bg-[#FDF3F0] group hover:bg-[--peach-dark]  transition flex flex-col max-w-[32.7rem] rounded-3xl text-center overflow-hidden md:flex-row md:max-w-[68.9rem] lg:flex-col">
+      <Image
+        className="md:max-w-[33.9rem] lg:max-w-[35rem]"
+        src={props.image}
+        alt="Project preview"
+      />
+      <div className="px-[3rem] py-[3.2rem] transition  group-hover:text-white md:self-center">
+        <h3 className="uppercase text-[2rem] tracking-[.5rem] leading-[2.6rem] text-[--peach-dark] mb-[1.6rem] group-hover:text-white">
+          {props.title}
+        </h3>
+        <p className="body-text group-hover:text-white">{props.description}</p>
+      </div>
+    </article>
+  );
+};
+
+export default ProjectCard;
