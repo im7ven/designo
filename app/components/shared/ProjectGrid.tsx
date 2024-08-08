813b09d6565d575a5790eea035e8611e47adb4d0
Justin <alexanderrr416@gmail.com>
2024-08-08 18:28:18 -0400

    style: adjust padding

--- app/components/shared/ProjectGrid.tsx
+++ app/components/shared/ProjectGrid.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ProjectGrid = ({ projects }: Props) => {
   return (
-    <section className="grid gap-[4rem] justify-center px-[2.4rem] my-[9.6rem] md:my-[12rem] md:gap-[3.2rem] lg:my-[16rem] lg:grid-cols-3 lg:px-0 lg:gap-[3rem]">
+    <section className="grid gap-[4rem] justify-center px-[2.4rem] my-[9.6rem] md:my-[12rem] md:gap-[3.2rem] lg:my-[16rem] lg:grid-cols-3 xl:px-0 lg:gap-[3rem]">
       {projects.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
