3d39304746805096162ab9f103e3e204271721d9
Justin <alexanderrr416@gmail.com>
2024-08-10 21:20:42 -0400

    feat: build contact details component

---
+++ app/components/contact/ContactDetails.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ContactDetails = () => {
+  return (
+    <div className="text-center md:text-left lg:self-center">
+      <h1 className="mb-[2.4rem] lg:mb-[3.2rem]">Contact Us</h1>
+      <p className="body-text text-white">
+        Ready to take it to the next level? Let’s talk about your project or
+        idea and find out how we can help your business grow. If you are looking
+        for unique digital experiences that’s relatable to your users, drop us a
+        line.
+      </p>
+    </div>
+  );
+};
+
+export default ContactDetails;
