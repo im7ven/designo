c2b2867c2b91394e3f65eb5da120be6a6e4de6f9
Justin <alexanderrr416@gmail.com>
2024-08-10 21:48:58 -0400

    style: apply background design for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,8 +6,9 @@ import { realDealFeature, talentFeature } from "../data/AboutFeatureSections";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="relative">
       <main className="max-w-[111rem] mx-auto md:px-[2.4rem] xl:px-0">
+        <div className="about-design1 w-[100.6rem] h-[59.2rem]"></div>
         <AboutHero />
         <AboutFeature {...talentFeature} />
         <LocationGrid />
@@ -15,6 +16,7 @@ const AboutPage = () => {
           <AboutFeature {...realDealFeature} />
         </div>
       </main>
+      <div className="about-design2 w-[100.6rem] h-[59.2rem]"></div>
       <Footer />
     </div>
   );
